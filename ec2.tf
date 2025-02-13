6d069ba5b15feaf83d4517944705619e36d0c778
Olakunle Oladepo <oladepokunle@gmail.com>
2025-02-13 12:12:39 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,8 @@
 resource "aws_instance" "storage-srv" {
    ami = "ami-085ad6ae776d8f09c"
   instance_type = "t2.micro"
-  key_name = "SrvKeyPair"
-  security_groups = [ "InstanceSecGrp" ]
+  key_name = ""
+  security_groups = [ "" ]
   availability_zone = "us-east-1a"
   associate_public_ip_address = "true"
   iam_instance_profile = aws_iam_instance_profile.ec2instance.name
